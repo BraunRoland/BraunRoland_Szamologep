922693a86dd419e2d8c9b3973e96d18faf205f99
Braun Roland <rolibraun94@gmail.com>
2025-01-19 23:36:06 +0100

    MasodFoku() elkészítve

--- BraunRoland_Szamologep/Program.cs
+++ BraunRoland_Szamologep/Program.cs
@@ -55,6 +55,19 @@ namespace BraunRoland_Szamologep
             }
         }
 
+        static void MasodFoku()
+        {
+            Console.WriteLine("Add meg az 'a' értékét: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a 'b' értékét: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a 'c' értékét: ");
+            int c = int.Parse(Console.ReadLine());
+            double x1 = (-b + (Math.Sqrt(Math.Pow(b,2) - 4 * a * c))) / (2 * a);
+            double x2 = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+            Console.WriteLine($"Első megoldás: {x1} |   Második megoldás: {x2}");
+        }
+
         static void Main(string[] args)
         {
         }
